af7b3e8313c336167cffa6c3ddd38fafbe83c208
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2021-05-04 23:51:56 +0200

    Fix GPIO reading
    * Use interupt for GPIO detection

--- home.admin/tickerEink.py
+++ home.admin/tickerEink.py
@@ -18,6 +18,11 @@ from PIL import ImageDraw
 shutting_down = False
 picdir = os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets'), 'images')
 
+BUTTON_GPIO_1 = 5
+BUTTON_GPIO_2 = 6
+BUTTON_GPIO_3 = 13
+BUTTON_GPIO_4 = 19
+
 def internet(host="8.8.8.8", port=53, timeout=3):
     """
     Host: 8.8.8.8 (google-public-dns-a.google.com)
@@ -84,6 +89,54 @@ def init_logging():
     handler = logging.StreamHandler(sys.stdout)
     logger.addHandler(handler)
 
+def setup_GPIO():
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(BUTTON_GPIO_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BUTTON_GPIO_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BUTTON_GPIO_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BUTTON_GPIO_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    GPIO.add_event_detect(BUTTON_GPIO_1, GPIO.FALLING, 
+                          callback=button_pressed_callback1, bouncetime=100)
+    GPIO.add_event_detect(BUTTON_GPIO_2, GPIO.FALLING, 
+                          callback=button_pressed_callback2, bouncetime=100)
+    GPIO.add_event_detect(BUTTON_GPIO_3, GPIO.FALLING, 
+                          callback=button_pressed_callback3, bouncetime=100)
+    GPIO.add_event_detect(BUTTON_GPIO_4, GPIO.FALLING, 
+                          callback=button_pressed_callback4, bouncetime=100)
+        
+
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+
+def button_pressed_callback1(channel):
+    global key1state
+    key1state = False
+
+def button_pressed_callback2(channel):
+    global key2state
+    key2state = False
+
+def button_pressed_callback3(channel):
+    global key3state
+    key3state = False
+
+def button_pressed_callback4(channel):
+    global key4state
+    key4state = False
+
+def clear_state():
+    global key1state
+    global key2state
+    global key3state
+    global key4state
+    key1state = True
+    key2state = True
+    key3state = True
+    key4state = True
+
 
 def main():
     ticker = Ticker()
@@ -107,6 +160,12 @@ def main():
 
 
     global shutting_down
+    global key1state
+    global key2state
+    global key3state
+    global key4state    
+
+    clear_state()
 
     atexit.register(shutdown_hook)
     signal.signal(signal.SIGTERM, signal_hook)
@@ -114,17 +173,9 @@ def main():
     if True:
         logging.info("epd2in7 BTC ticker")
 
-        GPIO.setmode(GPIO.BCM)
-        key1 = 5
-        key2 = 6
-        key3 = 13
-        key4 = 19
-
-        GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+        setup_GPIO()
+        signal.signal(signal.SIGINT, signal_handler)
+   
 
 #       Note that there has been no data pull yet
         datapulled=False
@@ -136,27 +187,12 @@ def main():
         notifier = sdnotify.SystemdNotifier()
         notifier.notify("READY=1")
      
-        while True:
+        while True:         
+            
             if shutting_down:
                 logging.info("App is shutting down.....")
                 break
             display_update = False
-            GPIO.setmode(GPIO.BCM)
-            key1 = 5
-            key2 = 6
-            key3 = 13
-            key4 = 19
-
-            GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-            key1state = GPIO.input(key1)
-            key2state = GPIO.input(key2)
-            key3state = GPIO.input(key3)
-            key4state = GPIO.input(key4)
 
             notifier.notify("WATCHDOG=1")
             if internet():
@@ -166,37 +202,48 @@ def main():
                     if last_mode_ind >= len(mode_list):
                         last_mode_ind = 0
                     lastcoinfetch=fullupdate(mode_list[last_mode_ind], days_list[days_ind])
-                    display_update = True                    
+                    display_update = True     
+                    clear_state()
+                    setup_GPIO()
                 elif key2state == False:
                     logging.info('Key2')
                     days_ind += 1
                     if days_ind >= len(days_list):
                         days_ind = len(days_list) - 1
                     lastcoinfetch=fullupdate(mode_list[last_mode_ind], days_list[days_ind])
-                    display_update = True                    
+                    display_update = True  
+                    clear_state()
+                    setup_GPIO()
                 elif key3state == False:
                     logging.info('Key3')
                     lastcoinfetch = fullupdate("newblock", days_list[days_ind])
                     display_update = True
                     newblock_displayed = True
+                    clear_state()
+                    setup_GPIO()
                 elif key4state == False:
                     logging.info('Key4')
                     lastcoinfetch = fullupdate(mode_list[last_mode_ind], days_list[days_ind])
                     display_update = True
+                    clear_state()
+                    setup_GPIO()
                 if (time.time() - lastheightfetch > 10):
                     new_height = ticker.mempool.getBlockHeight()
                     if new_height > height and not display_update:
                         lastcoinfetch = fullupdate("newblock", days_list[days_ind])
                         newblock_displayed = True
+                        setup_GPIO()
                     height = new_height
                     lastheightfetch = time.time()
                     
                 if (time.time() - lastcoinfetch > updatefrequency) or (datapulled==False):
                     lastcoinfetch=fullupdate(mode_list[last_mode_ind], days_list[days_ind])
                     datapulled = True
+                    setup_GPIO()
                 elif newblock_displayed and (time.time() - lastcoinfetch > 150):
                     lastcoinfetch=fullupdate(mode_list[last_mode_ind], days_list[days_ind])
-                    datapulled = True                    
+                    datapulled = True
+                    setup_GPIO()
                 else:
                     time.sleep(0.5)
 
