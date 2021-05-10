2a808c765a6af930535e7e385c8549b4088cd443
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2021-05-10 15:10:09 +0200

    Add update script

---
+++ home.admin/99updateMenu.sh
@@ -0,0 +1,199 @@
+# Taken from https://github.com/rootzoll/raspiblitz/blob/v1.7/home.admin/99updateMenu.sh
+#!/bin/bash
+
+# load btc-ticker config data
+source /home/admin/btc-ticker.info
+source /home/admin/_version.info
+
+## PROCEDURES
+
+release()
+{
+  whiptail --title "Update Instructions" --yes-button "Not Now" --no-button "Start Update" --yesno "To update your btc-ticker to a new version:
+
+- Download the new SD card image to your laptop:
+  https://github.com/btc-ticker/btc-ticker
+- Flash that SD card image to a new SD card (best)
+  or override old SD card after shutdown (fallback) 
+- Choose 'Start Update' below.
+
+No need to close channels or download blockchain again.
+Do you want to start the Update now?
+      " 16 62
+  if [ $? -eq 0 ]; then
+    exit 1
+  fi
+
+
+  whiptail --title "READY TO UPDATE?" --yes-button "START UPDATE" --no-button "Cancel" --yesno "If you start the update: The btc-ticker will power down.
+Once the LCD is white and no LEDs are blicking anymore:
+
+- Remove the Power from btc-ticker
+- Exchange the old with the new SD card
+- Connect Power back to the btc-ticker
+
+Do you have the SD card with the new version image ready
+and do you WANT TO START UPDATE NOW?
+      " 16 62
+
+  if [ $? -eq 1 ]; then
+    dialog --title " Update Canceled " --msgbox "
+OK. btc-ticker will NOT update now.
+      " 7 39
+    exit 1
+  fi
+
+  clear
+  sudo shutdown now
+}
+
+patchNotice()
+{
+  whiptail --title "Patching Notice" --yes-button "Dont Patch" --no-button "Patch Menu" --yesno "This is the possibility to patch your btc-ticker:
+It means it will sync the program code with the
+GitHub repo for your version branch v${codeVersion}.
+
+This can be useful if there are important updates 
+in between releases to fix severe bugs. It can also
+be used to sync your own code with your btc-ticker 
+if you are developing on your own GitHub Repo.
+
+BUT BEWARE: This means btc-ticker will contact GitHub,
+hotfix the code and might compromise your security.
+
+Do you want to Patch your btc-ticker now?
+      " 18 58
+  if [ $? -eq 0 ]; then
+    exit 1
+  fi
+}
+
+patch()
+{
+
+  # get sync info
+  source <(sudo /home/admin/XXsyncScripts.sh info)
+
+  # Patch Options
+  OPTIONS=(PATCH "Patch/Sync btc-ticker with GitHub Repo" \
+           REPO "Change GitHub Repo to sync with" \
+           BRANCH "Change GitHub Branch to sync with" \
+           PR "Checkout a PullRequest to test"
+        )
+
+  CHOICE=$(whiptail --clear --title "GitHub-User: ${activeGitHubUser} Branch: ${activeBranch}" --menu "" 11 55 4 "${OPTIONS[@]}" 2>&1 >/dev/tty)
+
+  clear
+  case $CHOICE in
+    PATCH)
+      sudo -u admin /home/admin/XXsyncScripts.sh -run
+      sleep 4
+      whiptail --title " Patching/Syncing " --yes-button "Reboot" --no-button "Skip Reboot" --yesno "  OK patching/syncing done.
+
+  By default a reboot is advised.
+  Only skip reboot if you know
+  it will work without restart.
+      " 11 40
+      if [ $? -eq 0 ]; then
+        clear
+        echo "REBOOT .."
+        sudo reboot
+        sleep 8
+      else
+        echo "SKIP REBOOT .."
+      fi
+      exit 1
+      ;;
+    REPO)
+      clear
+      echo "..."
+      newGitHubUser=$(whiptail --inputbox "\nPlease enter the GitHub USERNAME of the forked btc-ticker Repo?" 10 38 ${activeGitHubUser} --title "Change Sync Repo" 3>&1 1>&2 2>&3)
+      exitstatus=$?
+      if [ $exitstatus = 0 ]; then
+        newGitHubUser=$(echo "${newGitHubUser}" | cut -d " " -f1)
+        echo "--> " ${newGitHubUser}
+        error=""
+        source <(sudo -u admin /home/admin/XXsyncScripts.sh -clean ${activeBranch} ${newGitHubUser})
+        if [ ${#error} -gt 0 ]; then
+          whiptail --title "ERROR" --msgbox "${error}" 8 30
+        fi
+      fi
+      patch
+      exit 1
+      ;;
+    BRANCH)
+      clear
+      echo "..."
+      newGitHubBranch=$(whiptail --inputbox "\nPlease enter the GitHub BRANCH of the btc-ticker Repo '${activeGitHubUser}'?" 10 38 ${activeBranch} --title "Change Sync Branch" 3>&1 1>&2 2>&3)
+      exitstatus=$?
+      if [ $exitstatus = 0 ]; then
+        newGitHubBranch=$(echo "${newGitHubBranch}" | cut -d " " -f1)
+        echo "--> " $newGitHubBranch
+        error=""
+        source <(sudo -u admin /home/admin/XXsyncScripts.sh ${newGitHubBranch})
+        if [ ${#error} -gt 0 ]; then
+          whiptail --title "ERROR" --msgbox "${error}" 8 30
+        fi
+      fi
+      patch
+      exit 1
+      ;;
+    PR)
+      clear
+      echo "..."
+      pullRequestID=$(whiptail --inputbox "\nPlease enter the NUMBER of the PullRequest on btc-ticker Repo '${activeGitHubUser}'?" 10 46 --title "Checkout PullRequest ID" 3>&1 1>&2 2>&3)
+      exitstatus=$?
+      if [ $exitstatus = 0 ]; then
+        pullRequestID=$(echo "${pullRequestID}" | cut -d " " -f1)
+        echo "# --> " $pullRequestID
+        cd /home/admin/btc-ticker
+        git fetch origin pull/${pullRequestID}/head:pr${pullRequestID}
+        error=""
+        source <(sudo -u admin /home/admin/XXsyncScripts.sh pr${pullRequestID})
+        if [ ${#error} -gt 0 ]; then
+          whiptail --title "ERROR" --msgbox "${error}" 8 30
+        else
+          echo "# update installs .."
+          /home/admin/XXsyncScripts.sh -justinstall
+        fi
+      fi
+      exit 1
+      ;;
+  esac
+
+
+
+# quick call by parameter
+if [ "$1" == "github" ]; then
+  patch
+  exit 0
+fi
+
+# Basic Options Menu
+HEIGHT=10 # add 6 to CHOICE_HEIGHT + MENU lines
+WIDTH=55
+CHOICE_HEIGHT=4 # 1 line / OPTIONS
+OPTIONS=(
+RELEASE "btc-ticker Release Update/Recovery"
+PATCH "Patch btc-ticker v${codeVersion}"
+)
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "" \
+                --title "Update Options" \
+                --ok-label "Select" \
+                --cancel-label "Main menu" \
+                --menu "" \
+          $HEIGHT $WIDTH $CHOICE_HEIGHT \
+          "${OPTIONS[@]}" 2>&1 >/dev/tty)
+
+case $CHOICE in
+  RELEASE)
+    release
+    ;;
+  PATCH)
+    patchNotice
+    patch
+    ;;
+esac
---
+++ home.admin/XXsyncScripts.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+# taken from https://raw.githubusercontent.com/rootzoll/raspiblitz/v1.7/home.admin/XXsyncScripts.sh
+
+# command info
+if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "-help" ]; then
+  echo "FOR DEVELOPMENT USE ONLY!"
+  echo "btc-ticker Sync Scripts"
+  echo "XXsyncScripts.sh info"
+  echo "XXsyncScripts.sh [-run|-clean|-install|-justinstall] branch [repo]"
+  exit 1
+fi
+
+cd /home/admin/btc-ticker
+# gather info
+activeGitHubUser=$(sudo -u admin cat /home/admin/btc-ticker/.git/config 2>/dev/null | grep "url = " | cut -d "=" -f2 | cut -d "/" -f4)
+activeBranch=$(git branch 2>/dev/null | grep \* | cut -d ' ' -f2)
+
+# if parameter is "info" just give back basic info about sync
+if [ "$1" == "info" ]; then
+  echo "activeGitHubUser='${activeGitHubUser}'"
+  echo "activeBranch='${activeBranch}'"
+  exit 1
+fi
+
+# change branch if set as parameter
+vagrant=0
+clean=0
+install=0
+wantedBranch="$1"
+wantedGitHubUser="$2"
+if [ "${wantedBranch}" = "-run" ]; then
+  # "-run" ist just used by "patch" command and will ignore all further parameter
+  wantedBranch="${activeBranch}"
+  wantedGitHubUser="${activeGitHubUser}"
+  # detect if running in vagrant VM
+  vagrant=$(df | grep -c "/vagrant")
+  if [ "$2" = "git" ]; then 
+    echo "# forcing guthub over vagrant sync"
+    vagrant=0
+  fi
+fi
+if [ "${wantedBranch}" = "-clean" ]; then
+  clean=1
+  wantedBranch="$2"
+  wantedGitHubUser="$3"
+fi
+if [ "${wantedBranch}" = "-install" ]; then
+  install=1
+  wantedBranch="$2"
+  wantedGitHubUser="$3"
+fi
+if [ "${wantedBranch}" = "-justinstall" ]; then
+  clean=1
+  install=1
+  wantedBranch=""
+  wantedGitHubUser=""
+fi
+
+# set to another GutHub repo as origin
+if [ ${#wantedGitHubUser} -gt 0 ] && [ ${vagrant} -eq 0 ]; then
+  echo "# your active GitHubUser is: ${activeGitHubUser}"
+  echo "# your wanted GitHubUser is: ${wantedGitHubUser}"
+  if [ "${activeGitHubUser}" = "${wantedGitHubUser}" ]; then
+    echo "# OK"
+  else
+
+    echo "# checking repo exists .."
+    repoExists=$(curl -s https://api.github.com/repos/${wantedGitHubUser}/btc-ticker | jq -r '.name' | grep -c 'btc-ticker')
+    if [ ${repoExists} -eq 0 ]; then
+      echo "error='repo not found'"
+      exit 1
+    fi
+
+    echo "# try changing github origin .."
+    git remote set-url origin https://github.com/${wantedGitHubUser}/btc-ticker.git
+    activeGitHubUser=$(sudo -u admin cat /home/admin/btc-ticker/.git/config | grep "url = " | cut -d "=" -f2 | cut -d "/" -f4)
+  fi
+fi
+
+if [ ${#wantedBranch} -gt 0 ] && [ ${vagrant} -eq 0 ]; then
+  echo "# your active branch is: ${activeBranch}"
+  echo "# your wanted branch is: ${wantedBranch}"
+  if [ "${wantedBranch}" = "${activeBranch}" ]; then
+    echo "# OK"
+  else
+
+    # always clean & install fresh on branch change
+    clean=1
+    install=1
+
+    echo "# checking if branch is locally available"
+    localBranch=$(git branch | grep -c "${wantedBranch}")
+    if [ ${localBranch} -eq 0 ]; then
+      echo "# checking branch exists .."
+      branchExists=$(curl -s https://api.github.com/repos/${activeGitHubUser}/btc-ticker/branches/${wantedBranch} | jq -r '.name' | grep -c ${wantedBranch})
+      if [ ${branchExists} -eq 0 ]; then
+        echo "error='branch not found'"
+        exit 1
+      fi
+      echo "# checkout/changing branch .."
+      git fetch
+      git checkout -b ${wantedBranch} origin/${wantedBranch}
+    else
+      echo "# changing branch .."
+      git checkout ${wantedBranch}
+    fi
+
+    activeBranch=$(git branch | grep \* | cut -d ' ' -f2)
+  fi
+fi
+
+if [ ${vagrant} -eq 0 ]; then
+  origin=$(git remote -v | grep 'origin' | tail -n1)
+  echo "# *** SYNCING btc-ticker CODE WITH GITHUB ***"
+  echo "# This is for developing on your btc-ticker."
+  echo "# THIS IS NOT THE REGULAR UPDATE MECHANISM"
+  echo "# and can lead to dirty state of your scripts."
+  echo "# REPO ----> ${origin}"
+  echo "# BRANCH --> ${activeBranch}"
+  echo "# ******************************************"
+  git pull 1>&2
+  cd ..
+else
+  cd ..
+  echo "# --> VAGRANT IS ACTIVE"
+  echo "# *** SYNCING btc-ticker CODE WITH VAGRANT LINKED DIRECTORY ***"
+  echo "# This is for developing on your btc-ticker with a VM."
+  echo "# - delete /home/admin/btc-ticker"
+  sudo rm -r /home/admin/btc-ticker
+  sudo mkdir /home/admin/btc-ticker
+  echo "# - copy from vagrant new btc-ticker files (ignore hidden dirs)"
+  sudo cp -r /vagrant/* /home/admin/btc-ticker
+  echo "# - set admin as owner of files"
+  sudo chown admin:admin -R /home/admin/btc-ticker
+fi
+
+if [ ${clean} -eq 1 ]; then
+  echo "# Cleaning assets .. "
+  sudo rm -f *.sh
+  sudo rm -rf assets
+  sudo -u admin mkdir assets
+else
+  echo "# ******************************************"
+  echo "# NOT cleaning/deleting old files"
+  echo "# use parameter '-clean' if you want that next time"
+  echo "# ******************************************"
+fi
+
+echo "# Backup config.ini"
+sudo -u admin cp -f /home/admin/config.ini /home/admin/config.ini.backup
+
+echo "# COPYING from GIT-Directory to /home/admin/"
+sudo rm -r /home/admin/config.scripts
+sudo -u admin cp -r -f /home/admin/btc-ticker/home.admin/*.* /home/admin
+sudo -u admin cp -r -f /home/admin/btc-ticker/home.admin/assets /home/admin
+sudo -u admin chmod +x /home/admin/*.sh
+sudo -u admin chmod +x /home/admin/*.py
+sudo -u admin chmod +x /home/admin/config.scripts/*.sh
+sudo -u admin chmod +x /home/admin/config.scripts/*.py
+echo "# ******************************************"
+
+cd /home/admin/btc-ticker/
+sudo -H python3 setup.py install
+
+echo "# ******************************************"
+echo "# OK - shell scripts and assets are synced"
+echo "# Will restart btc-ticker now"
+echo "# If it does not work, please reboot"
+sudo -h sudo systemctl restart btcticker
