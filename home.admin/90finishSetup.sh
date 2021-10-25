47301c7e49516136e597a49260af915db818d5e9
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2021-10-25 15:50:33 +0200

    Fix bash append

--- home.admin/90finishSetup.sh
+++ home.admin/90finishSetup.sh
@@ -47,58 +47,31 @@ sudo ln -s /tmp /var/lock
 sudo mv /etc/resolv.conf /tmp/
 sudo ln -s /tmp/resolv.conf /etc/resolv.conf
 
-
-if ! grep -Eq '^tmpfs.*/var/log' /etc/fstab; then
-
-  if grep -Eq '/var/log' /etc/fstab; then
-    # entry is in file but most likely just disabled -> re-enable it
-    sudo sed -i -E 's|^#(tmpfs.*/var/log.*)$|\1|g' /etc/fstab
-  else
-    # missing -> add
-    echo "" | sudo tee -a /etc/fstab >/dev/null
-    echo "tmpfs         /var/log  tmpfs  nodev,nosuid  0  0" | sudo tee -a /etc/fstab >/dev/null
-  fi
-fi
-
-if ! grep -Eq '^tmpfs.*/var/tmp' /etc/fstab; then
-
-  if grep -Eq '/var/tmp' /etc/fstab; then
-    # entry is in file but most likely just disabled -> re-enable it
-    sudo sed -i -E 's|^#(tmpfs.*/var/tmp.*)$|\1|g' /etc/fstab
-  else
-    # missing -> add
-    echo "" | sudo tee -a /etc/fstab >/dev/null
-    echo "tmpfs         /var/tmp  tmpfs  nodev,nosuid  0  0" | sudo tee -a /etc/fstab >/dev/null
-  fi
-fi
-
-if ! grep -Eq '^tmpfs.*/tmp' /etc/fstab; then
-
-  if grep -Eq '/tmp' /etc/fstab; then
-    # entry is in file but most likely just disabled -> re-enable it
-    sudo sed -i -E 's|^#(tmpfs.*/tmp.*)$|\1|g' /etc/fstab
-  else
-    # missing -> add
-    echo "" | sudo tee -a /etc/fstab >/dev/null
-    echo "tmpfs         /tmp  tmpfs  nodev,nosuid  0  0" | sudo tee -a /etc/fstab >/dev/null
-  fi
-fi
-
+echo "tmpfs         /var/log  tmpfs  nodev,nosuid  0  0
+tmpfs         /var/tmp  tmpfs  nodev,nosuid  0  0
+tmpfs         /tmp  tmpfs  nodev,nosuid  0  0
+" | sudo tee -a /etc/fstab
 
 sudo sed -i /etc/fstab -e "s/\(.*\/boot.*vfat.*\)defaults\(.*\)/\1defaults,ro\2/"
 sudo sed -i /etc/fstab -e "s/\(.*\/.*ext4.*\)defaults\(.*\)/\1defaults,ro\2/"
 
 sudo sed -i /boot/cmdline.txt -e "s/\(.*fsck.repair=yes.*\)rootwait\(.*\)/\1rootwait fastboot noswap\2/"
 
-echo "set_bash_prompt(){" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "fs_mode=$(mount | sed -n -e "s/^\/dev\/.* on \/ .*(\(r[w|o]\).*/\1/p")" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "PS1='\[\033[01;32m\]\u@\h${fs_mode:+($fs_mode)}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "}" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "alias ro='sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot'" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "alias rw='sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot'" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "" | sudo tee -a /etc/bash.bashrc >/dev/null
-echo "PROMPT_COMMAND=set_bash_prompt" | sudo tee -a /etc/bash.bashrc >/dev/null
+
+echo "
+set_bash_prompt(){
+" | sudo tee -a /etc/bash.bashrc
+echo 'fs_mode=$(mount | sed -n -e "s/^\/dev\/.* on \/ .*(\(r[w|o]\).*/\1/p")
+' | sudo tee -a /etc/bash.bashrc
+echo "PS1='\[\033[01;32m\]\u@\h\${fs_mode:+(\$fs_mode)}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '
+}
+
+alias ro='sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot'
+alias rw='sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot'
+
+PROMPT_COMMAND=set_bash_prompt
+" | sudo tee -a /etc/bash.bashrc
+
 
 # mark setup is done
 sudo sed -i "s/^setupStep=.*/setupStep=100/g" /home/admin/btc-ticker.info
