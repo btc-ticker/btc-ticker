c1104b71110bf944c1dd618007ceee1a1368a08d
Holger <holgern@users.noreply.github.com>
2025-07-14 19:57:52 +0200

    Update packer.build.armhf-rpi.sh

--- ci/armhf_rpi/packer.build.armhf-rpi.sh
+++ ci/armhf_rpi/packer.build.armhf-rpi.sh
@@ -7,7 +7,7 @@ set_variables "$@"
 # build the image in docker
 echo -e "\nBuild the image..."
 # from https://hub.docker.com/r/mkaczanowski/packer-builder-arm/tags
-command="docker run --rm --privileged -v /dev:/dev -v ${PWD}:/build \
+command="docker run -e QEMU_CPU=arm1176 --rm --privileged -v /dev:/dev -v ${PWD}:/build \
   mkaczanowski/packer-builder-arm@sha256:023b9d33bce0834267bdbfab17e6f19b07712dfb484646d493a6af8a14780bba \
   build ${vars} build.armhf-rpi.pkr.hcl"
 echo "# Running: $command"
