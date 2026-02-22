8a60de7345a6043faab1dff3b7ef43d958783ecd
Holger <holgern@users.noreply.github.com>
2026-02-22 17:43:51 +0100

    Update Python dependencies in build_sdcard.sh
    Added python3-rpi.gpio and python3-pydantic to the installation of Python dependencies. Updated pip install command to include RPi.GPIO and pydantic.

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -644,7 +644,7 @@ usermod -a -G i2c admin
 echo -e "\n*** Python DEFAULT libs & dependencies ***"
 
 # for setup shell scripts
-apt -y install dialog bc python3-dialog
+apt -y install dialog bc python3-dialog  python3-rpi.gpio python3-pydantic
 
 # libs (for global python scripts)
 sudo -u admin python3 -m venv /home/admin/.venv
@@ -657,7 +657,7 @@ sudo -u admin /home/admin/.venv/bin/pip install mplfinance==0.12.10b0
 # sudo -u admin /home/admin/.venv/bin/pip install wtforms
 # sudo -u admin /home/admin/.venv/bin/pip install gunicorn
 
-sudo -u admin /home/admin/.venv/bin/pip install sdnotify
+sudo -u admin /home/admin/.venv/bin/pip install sdnotify RPi.GPIO pydantic
 
 # *** fail2ban ***
 # based on https://stadicus.github.io/RaspiBolt/raspibolt_21_security.html
