108511e9ab091c1cd838d13a91629c27fcb76852
holgern <holgern@users.noreply.github.com>
2026-03-14 13:46:31 +0100

    feat: helper scripts

---
+++ home.admin/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/admin/.venv/bin/btcticker --config /home/admin/config.ini download --fontdir /home/admin/assets/fonts
---
+++ home.admin/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/home/admin/.venv/bin/pip install btcticker --upgrade
+/home/admin/.venv/bin/pip install piltext --upgrade
+/home/admin/.venv/bin/pip install pymempool --upgrade
+/home/admin/.venv/bin/pip install pyccxt --upgrade
